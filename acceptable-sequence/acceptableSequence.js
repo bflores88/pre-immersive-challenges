b697fc8399dfcb6043c751fd897a01975aaf5f62
kmietotwy <kmietotwy@gmail.com>
2019-03-20 16:54:19 -1000

    acceptable seq

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,16 @@
 module.exports = function acceptableSequence(str) {
-  // write your code here
+  var testFor = '+';
+
+  for (var i= 0; i<str.length; i++){
+    
+    if(str[i] !== '+' && str[i] !== '=' ){
+
+      if(str.charAt(i-1) === testFor && str.charAt(i+1) === testFor){
+        return true
+      } else { return false; }
+
+    }
+
+  }
+
 }
