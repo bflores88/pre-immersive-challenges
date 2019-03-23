919760d7a0febf3f41315807298bb565049da188
kmietotwy <kmietotwy@gmail.com>
2019-03-22 16:24:44 -1000

    gimme cookies

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,17 @@
 module.exports = function cookieProblem(array) {
-  // write your code in here
+  var addCookies = 0;
+  var mostCookies = 0;
+
+  for(var i=0; i<array.length; i++){
+    if(array[i] > mostCookies){
+      mostCookies = array[i];
+    }
+  }
+
+  array.forEach(function(elem){
+    var cookieDiff = mostCookies - elem;
+    addCookies+=cookieDiff;
+  })
+
+  return addCookies;
 }
