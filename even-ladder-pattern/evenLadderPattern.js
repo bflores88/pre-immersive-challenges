288d0ee7d3ac437f3aa21e8261304471e0b0dcb9
kmietotwy <kmietotwy@gmail.com>
2019-03-22 19:12:44 -1000

    even ladder pattern

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,14 @@
-module.exports = function evenLadderPattern(num) {
-  // write your code in here
+module.exports = function evenLadderPattern(num) {  
+  var numStr = '';
+  
+  for(var i=2; i<=num; i+=2){
+    var stringI = i.toString();
+    var repeat = stringI.repeat(stringI);
+    numStr = numStr.concat('\n' +repeat);
+
+  }
+  
+  var minStr = numStr.substr(1);
+  return minStr;
+
 }
