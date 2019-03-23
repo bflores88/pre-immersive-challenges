ef7763734693fa1b68be754a88e12d4059b5c2bd
kmietotwy <kmietotwy@gmail.com>
2019-03-22 16:16:25 -1000

    checked ascending order

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,9 @@
 module.exports = function checkAscOrder(numArray) {
-  // write your code in here
+  for(var i=0; i<numArray.length; i++){
+    if(numArray[i] < numArray[i+1]){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
