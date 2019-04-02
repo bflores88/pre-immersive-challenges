229fe246a9682ee1ed99c6654105a1d0279fdfa3
kmietotwy <kmietotwy@gmail.com>
2019-04-02 09:32:19 -1000

    middle value

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,3 +1,11 @@
 module.exports = function findMiddleValue(numArray) {
   // write your code in here
+  const maximum = Math.max(...numArray);
+  const minimum = Math.min(...numArray);
+  
+  for(let i=0; i<numArray.length; i++){
+    if(numArray[i] !== maximum && numArray[i] !== minimum){
+      return numArray.indexOf(numArray[i]);
+    }
+  }
 }
