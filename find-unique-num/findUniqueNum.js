684e4b7c56801b1fb99a6999189aaa22f3992545
kmietotwy <kmietotwy@gmail.com>
2019-04-08 19:02:19 -1000

    unique number using for loop

--- find-unique-num/findUniqueNum.js
+++ find-unique-num/findUniqueNum.js
@@ -1,3 +1,15 @@
 module.exports = function findUniqueNum(strOfNums) {
   // write your code in here
+  let unique;
+  let numArr = strOfNums.split(' ').map(function(elem){
+    return Number(elem);
+  });
+
+  for(let i=0; i<numArr.length; i++){
+    if(numArr[i]%2 !== numArr[i+1]%2 && numArr[i]%2 !== numArr[i-1]%2 && numArr[i]%2 !== numArr[i-2]%2){
+      unique = i;
+    }
+  }
+
+  return unique;
 }
