24d97bcebb5a4748386bfe5129d0d40a5a7de91f
kmietotwy <kmietotwy@gmail.com>
2019-04-08 18:20:02 -1000

    found longest word using loop

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,11 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
+  let longestWord = '';
+  for(let i=0; i<sen.length; i++){
+    if(sen[i].length > longestWord.length){
+      longestWord = sen[i];
+    }
+  }
+
+  return longestWord;
 }
