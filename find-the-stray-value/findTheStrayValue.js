ac833365dc1d3f997eee3e694ab211105b9017e8
kmietotwy <kmietotwy@gmail.com>
2019-04-08 18:26:52 -1000

    stray value using loop

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,12 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
+  let stray;
+  
+  for(let i=0; i<array.length; i++){
+    if(array[i] !== stray && array[i] !== array[i+1]  &  array[i] !== array[i-1]){
+      stray = array[i];
+    }
+  }
+
+  return stray;
 }
