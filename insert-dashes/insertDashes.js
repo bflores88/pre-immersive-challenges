fa9afb0ffb13f8253eca628bfd82b2b276457f79
kmietotwy <kmietotwy@gmail.com>
2019-04-09 14:01:37 -1000

    insert dashes using array chaining

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,14 @@
 module.exports = function insertDashes(num) {
   // write your code in here
+  let addDashes = num.toString()
+    .split('')
+    .reduce(function(previous, current){
+      if(parseInt(previous)%2 === 0 && parseInt(current)%2 === 0 && parseInt(current) !== 0){
+        return previous + '-' + current;
+      } else {
+        return previous + current;
+      }
+  })
+
+  return addDashes;
 }
